f21fc60d1dfdfc818c0046a1bda7b652d79bb5ba
HugoRoman01 <56384232+HugoRoman01@users.noreply.github.com>
2021-06-16 18:58:39 +0200

    Localizaciones y Personajes
    Creacion de los personajes y localizaciones iniciales y objetivos

--- src/Leer.java
+++ src/Leer.java
@@ -1,33 +1,46 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.Buffer;
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class Leer {
-    private  List<String> personajes = new ArrayList<String>();
+    private  List<Personajes> personajes = new ArrayList<Personajes>();
 	private  List<Lugares> localizaciones = new ArrayList<Lugares>();
-	private  List<String> objetos = new ArrayList<String>();
-
-	private  List<String> personajesLocIni = new ArrayList<String>();
-
-	private  List<String> locObjetos = new ArrayList<String>();
-	private  List<String> localizacionObjetivo = new ArrayList<String>();
+	private  List<Objetos> objetos = new ArrayList<Objetos>();
+	//private  List<String> personajesLocIni = new ArrayList<String>();
+	//private  List<String> locObjetos = new ArrayList<String>();
+	//private  List<String> localizacionObjetivo = new ArrayList<String>();
 
 
     public Leer(){
         int entrada = 0;
         String linea;
+        String linea2;
         ArrayList<String> lista_txt_inicial = new ArrayList<String>();//Lista para guardar el documento conla configuraci�n inicial
+        ArrayList<String> lista_final = new ArrayList<String>();
 
-        //LEEMOS EL FICHERO INICIAL    
         try{
+             //LEEMOS EL FICHERO INICIAL    
 
             String inic_state = System.getProperty("user.dir") + "\\documentos\\inic_state.txt";//Path del documento inicial
             FileReader f = new FileReader(inic_state);
             BufferedReader b = new BufferedReader(f);
 
+            //LEER EL FICHERO DE OBJETIVOS Y ENLISTARLOS
+
+            String objetivos = System.getProperty("user.dir") + "\\documentos\\objetivos.txt";
+            FileReader f2 = new FileReader(objetivos);
+            BufferedReader b2 = new BufferedReader(f2);
+
+            //CREAMOS LISTS FINAL
+            while((linea2 = b2.readLine()) != null){
+                lista_final.add(linea2);
+            }
+
+            //CREAMOS LISTS INICIAL
             while ((linea = b.readLine()) != null) {
                 lista_txt_inicial.add(linea);           
             }
@@ -45,12 +58,11 @@ public class Leer {
                 }else{
 
                     if(entrada ==0){
-                        establecer_localizaciones(elemento,localizaciones);
-
+                        crear_localizaciones(elemento,localizaciones);
                     }else if(entrada == 1){
-
+                        crear_personajes(elemento,personajes,lista_final);
                     }else if(entrada == 2){
-
+                        crear_objetos(elemento, objetos,lista_final);
                     }
 
                 }
@@ -63,10 +75,10 @@ public class Leer {
     
     }
 
-    static void establecer_localizaciones(String elemento, List<Lugares> localizaciones){
+
+    static void crear_localizaciones(String elemento, List<Lugares> localizaciones){
         
         ArrayList<String> lista_aux = new ArrayList<String>();
-
 
         String[] temp= elemento.replace(")"," ").trim().split("\\(");
         String lugar=temp[0];
@@ -74,7 +86,36 @@ public class Leer {
         for(String a:adyacentes) {lista_aux.add(a);}
         localizaciones.add(new Lugares(lugar,lista_aux));
     }
-    
+
+    static void crear_personajes(String elemento, List<Personajes> personajes,List<String> lista_final){
+
+        for(String elemento_final : lista_final ){
+
+            String[] temp= elemento.replace(")"," ").trim().split("\\(");
+            String nombrePJ = temp[0];
+            String posicion = temp[1].trim(); 
+
+            String[] temp2= elemento_final.replace(")"," ").trim().split("\\(");
+            String posicion_final = temp2[1].trim();
+
+            personajes.add(new Personajes(nombrePJ,posicion,posicion_final));
+
+        }
+    }
+
+    static void crear_objetos(String elemento, List<Objetos> objetos,List<String> lista_final){
+
+        for(String elemento_final : lista_final ){
+
+            
+
+
+
+
+        }
+
+
+    }
 }
     
 
--- src/Lugares.java
+++ src/Lugares.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Lugares {
-	private  List<String> adyacenciasLocalizaciones = new ArrayList<String>();
+	private List<String> adyacenciasLocalizaciones = new ArrayList<String>();
     private String lugar;
 
 
@@ -20,6 +20,14 @@ public class Lugares {
         return lugar;
     }
 
+    public void imprimir(){
+        System.out.println(this.getLugar()+ " esta conectado con: ");
+        for(String elemento : adyacenciasLocalizaciones) {
+			System.out.print(elemento + " ");
+		}
+		System.out.println();
+	
+    }
      
 
 
